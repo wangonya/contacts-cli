a79ad44f580e4274e5dbe4bbf473264d8efaae03
wangonya <kwangonya@gmail.com>
2019-08-23 07:04:32 +0300

    setup for publishing

--- setup.py
+++ setup.py
@@ -1,8 +1,24 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
         name="contacts",
+        version="0.1.0",
         py_modules=find_packages(),
+        description="Save contacts from your terminal",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/wangonya/contacts-cli",
+        author="Kinyanjui Wangonya",
+        author_email="kwangonya@gmail.com",
+        license="MIT",
+        classifiers=[
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.7",
+        ],
         install_requires=[
             "Click",
             "requests",
